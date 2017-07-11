e4648ab887b15a52398c56a923b036e35b93952d
ponpon <wizardsoloist1983@gmail.com>
2017-07-11 21:58:59 +0800

    add HitController

---
+++ Assets/Scripts/HitController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitController : MonoBehaviour {
+
+	public Text text;
+	
+	void OnMouseDown(){
+		text.text = "Click " + gameObject.tag;
+	}
+}
