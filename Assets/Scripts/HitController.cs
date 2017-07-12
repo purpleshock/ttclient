1af4ba5d74a0b8231bdef1e79a5f1fa524585465
ponpon <wizardsoloist1983@gmail.com>
2017-07-12 21:57:47 +0800

    add mobile touch

--- Assets/Scripts/HitController.cs
+++ Assets/Scripts/HitController.cs
@@ -6,6 +6,15 @@ using UnityEngine.UI;
 public class HitController : MonoBehaviour {
 
 	public Text text;
+
+	void Update(){
+		if (Input.touchCount > 0) {
+			Touch myTouch = Input.GetTouch (0);
+			if (myTouch.phase == TouchPhase.Ended) {
+				text.text = "Touch " + gameObject.tag;
+			}
+		}
+	}
 	
 	void OnMouseDown(){
 		text.text = "Click " + gameObject.tag;
