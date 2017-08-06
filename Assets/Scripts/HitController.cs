623f18722d5eac43daa1ff9624218fe75278c9d2
ponpon <wizardsoloist1983@gmail.com>
2017-08-06 12:55:06 +0800

    add currentSpriteName and RandomSprite() of HitController

--- Assets/Scripts/HitController.cs
+++ Assets/Scripts/HitController.cs
@@ -7,9 +7,16 @@ public class HitController : MonoBehaviour
 {
 	public Text text;
 	public Sprite spHead;
-	public Sprite spBreast;
+	public Sprite spChest;
 	public Sprite spBelly;
 	public Sprite spLowerBody;
+
+	private string currentSpriteName;
+
+	void Start ()
+	{
+		currentSpriteName = gameObject.tag;
+	}
 
 	void Update ()
 	{
@@ -32,7 +39,40 @@ public class HitController : MonoBehaviour
 	{
 		if (gameObject.tag != "Empty") {
 			SpriteRenderer spr = gameObject.GetComponent<SpriteRenderer> ();
-			spr.sprite = spHead;
+			spr.sprite = RandomSprite ();
 		}
+	}
+
+	Sprite RandomSprite ()
+	{
+		Sprite sprite = null;
+
+		List<string> spriteNameList = new List<string> ();
+		spriteNameList.Add ("Head");
+		spriteNameList.Add ("Chest");
+		spriteNameList.Add ("Belly");
+		spriteNameList.Add ("LowerBody");
+		spriteNameList.Remove (currentSpriteName);
+
+		int spriteIndex = Random.Range (0, spriteNameList.Count);
+
+		currentSpriteName = spriteNameList [spriteIndex];
+
+		switch (currentSpriteName) {
+		case "Head":
+			sprite = spHead;
+			break;
+		case "Chest":
+			sprite = spChest;
+			break;
+		case "Belly":
+			sprite = spBelly;
+			break;
+		case "LowerBody":
+			sprite = spLowerBody;
+			break;
+		}
+
+		return sprite;
 	}
 }
